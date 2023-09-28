78123cda62f16034742171d6d5dcd958767feaa5
Vedant <vedanthelwatkar@gmail.com>
2023-09-28 23:06:30 +0530

    commit

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoURI = "mongodb://localhost:27017/sculpt"
+const mongoURI = "mongodb+srv://vedanthelwatkar:vedd2201@cluster0.l2lripg.mongodb.net/"
 
 const connectToMongo = () => {
     // This is function to connect to mongoose. Connect function connect the mongoose to MongoDb thorugh the given URI. And if the connection is successfull, it returns a callback function and we can perform any operation to know that we have connected successfully(here we have performed console.log).
