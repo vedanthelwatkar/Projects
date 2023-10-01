488c29bd2620c1f521185b1a8087853dd73e0644
Vedant <vedanthelwatkar@gmail.com>
2023-10-02 00:07:34 +0530

    commit2

--- client/management_s/src/Entries.js
+++ client/management_s/src/Entries.js
@@ -4,12 +4,14 @@ import axios from "axios"
 export default function Entries(){
 
     const [info,setInfo] = useState([])
+    const [loading, setLoading] = useState(true);
 
     useEffect(()=>{
         let url = "https://vms-oyzr.onrender.com/entries"
         axios.get(url)
         .then(res=>{
             setInfo(res.data)
+            setLoading(false);
         })
         .catch(err=>{
             alert("Issue " + err)
@@ -31,40 +33,46 @@ export default function Entries(){
         })
 
     }
+    console.log(info);
 
-    return(
-        <>
-        <center>
+   return (
+    <>
+      <center>
         <h1>Entries</h1>
-
-        <a href="/" className="link">Home</a>
-		<table border="4" className="table">
-			<tr>
-			<th>	Name	</th>
-			<th>	Phone	</th>
-			<th>	Time	</th>
-			<th> 	Date	</th>
-            <th> 	Visitee	</th>
-			<th> 	Delete	</th>
-			</tr>
-			
-		{
-			info.map( (e) => (
-			<tr style={{textAlign : "center"}}>
-			<td>	{e.name}		</td>
-			<td>	{e.phone}	</td>
-			<td>	{e.time}	</td>
-            <td>	{e.date}	</td>
-            <td>	{e.visitee}	</td>
-			<td> <button onClick = { () => {if (window.confirm(' Are you sure you want to delete this entry? ')) delEnt(e.phone); }}>
-			Delete </button> </td>
-			</tr>
-
-		))
-	}
-
-		</table>	
-        </center>
-        </>
-    )
+        <a href="/" className="link">
+          Home
+        </a>
+        {loading ? (
+          // Display loading message or spinner while data is loading
+          <p>Loading...</p>
+        ) : (
+          <table border="4" className="table">
+            {/* Your table header */}
+            <tbody>
+              {info.map((e) => (
+                <tr style={{ textAlign: "center" }} key={e.phone}>
+                  {/* Your table data */}
+                  <td>{e.name}</td>
+                  <td>{e.phone}</td>
+                  <td>{e.time}</td>
+                  <td>{e.date}</td>
+                  <td>{e.visitee}</td>
+                  <td>
+                    <button
+                      onClick={() => {
+                        if (window.confirm("Are you sure you want to delete this entry?"))
+                          delEnt(e.phone);
+                      }}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
+      </center>
+    </>
+  );
 }
