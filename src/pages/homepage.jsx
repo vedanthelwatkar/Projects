09c1a893ab8364cfc80eeb76e5250636736ea6ec
Vedant <vedanthelwatkar@gmail.com>
2023-11-03 12:55:11 +0530

    commit

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -68,6 +68,19 @@ const Homepage = () => {
 		boxShadow: stayLogo ? "0px 4px 10px rgba(0, 0, 0, 0.25)" : "none",
 	};
 
+
+	const resume = ()=>{
+		let url = "https://drive.google.com/file/d/1QlCgwdYdDM0E58rV2CBvJn5-KIivEZK0/view?usp=drive_link"
+		window.open(url, '_blank');
+	}
+	const download = () => {
+		let url = "https://drive.google.com/uc?export=download&id=1QlCgwdYdDM0E58rV2CBvJn5-KIivEZK0";
+		let a = document.createElement('a');
+		a.href = url;
+		a.download = 'Resume Vedant_Helwatkar.pdf';
+		a.click();
+	}
+
 	return (
 		<React.Fragment>
 			<Helmet>
@@ -91,9 +104,23 @@ const Homepage = () => {
 					<div className="homepage-container">
 						<div className="homepage-first-area">
 							<div className="homepage-first-area-left-side">
-								<div className="title homepage-title">
-									{INFO.homepage.title}
-								</div>
+							<div className="title homepage-title">
+							{INFO.homepage.title}
+							<br></br>
+							<div className="button-container">
+							<button className="butt" onClick={resume}>
+								<span>Resume</span>
+							</button>
+							<button className="Btn" onClick={download}>
+								<svg className="svgIcon" viewBox="0 0 384 512" height="1em" xmlns="http://www.w3.org/2000/svg">
+								<path d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"></path>
+								</svg>
+								<span className="icon2"></span>
+								<span className="tooltip">Download</span>
+							</button>
+							</div>
+							</div>
+
 
 								<div className="subtitle homepage-subtitle">
 									{INFO.homepage.description}
@@ -161,6 +188,9 @@ const Homepage = () => {
 							</div>
 							<AllIntern />
 						</div>
+						<div className="page-footer">
+						<Footer />
+					</div>
 					</div>
 				</div>
 			</div>
