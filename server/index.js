147bf6c4a5e477891d3b9299efdb49cb5f468137
Vedant <vedanthelwatkar@gmail.com>
2023-11-21 13:26:51 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -13,7 +13,6 @@ const app = express();
 dotenv.config();
 app.use(cors())
 app.use('/api', createProxyMiddleware({ 
-    target: '*',
     changeOrigin: true, 
     //secure: false,
     onProxyRes: function (proxyRes, req, res) {
