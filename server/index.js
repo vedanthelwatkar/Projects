70f6e2816a16ecdd6726579c1249a6d26d183bc3
Vedant <vedanthelwatkar@gmail.com>
2023-11-21 18:27:09 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ import cookieParser from "cookie-parser";
 const app = express();
 dotenv.config();
 app.use(express.json());
-
+app.use(cors())
 const connect = () => {
   mongoose
     .connect(process.env.MONGO)
@@ -30,11 +30,6 @@ app.use("/api/comments", commentRoutes);
 app.use("/api/auth", authRoutes);
 
 app.use((err, req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://vtube-ytclone.vercel.app');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  res.header('Access-Control-Allow-Credentials', true);  // Enable credentials (cookies)
-  next();
   console.error("Error:", err);
 
   const status = err.status || 500;
