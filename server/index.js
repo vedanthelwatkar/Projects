3dc532c80fc9a6d1066e832c36f19c849874f8f8
Vedant <vedanthelwatkar@gmail.com>
2023-11-21 14:51:45 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -9,19 +9,26 @@ import authRoutes from "./routes/auth.js";
 import cookieParser from "cookie-parser";
 
 const app = express();
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://vtube-ytclone.vercel.app"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  // Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+  res.setHeader("Access-Control-Max-Age", 7200);
 
-// Use cors middleware for more concise setup
-app.use(
-  cors({
-    origin: "https://vtube-ytclone.vercel.app",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE",
-    allowedHeaders: "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers",
-    credentials: true,
-    exposedHeaders: ["Access-Control-Allow-Private-Network"],
-    maxAge: 7200,
-  })
-);
-
+  next();
+});
 dotenv.config();
 console.log('CORS middleware applied');
 app.use(express.json());
