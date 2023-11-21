9d616b07cd471f145aa505612c812fa055b9e37b
Vedant <vedanthelwatkar@gmail.com>
2023-11-21 14:07:51 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -10,8 +10,14 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 dotenv.config();
-app.use(cors())
-app.options('*', cors());
+const cors=require("cors");
+ const corsOptions ={
+       origin:'*', 
+       credentials:true, //access-control-allow-credentials:true
+        optionSuccessStatus:200,
+ }
+
+ app.use(cors(corsOptions)) 
 app.use(express.json());
 
 const connect = () => {
