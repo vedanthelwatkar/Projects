63729f88bf6d25ce42a6276372c4a61d12760eb0
Vedant <vedanthelwatkar@gmail.com>
2023-11-24 00:45:15 +0530

    commit

---
+++ routes/video.js
@@ -0,0 +1,32 @@
+import express from "express";
+import { addVideo, addView, deleteVideo, getBySelect, getByTags, getVideo, getVideos, random, search, sub, trend, updateVideo } from "../controllers/video.js";
+import { verifyToken } from "../verifyToken.js";
+
+const router = express.Router();
+
+router.post("/",addVideo)
+
+router.put("/:id",verifyToken,updateVideo)
+
+router.delete("/:id",deleteVideo)
+
+router.get("/find/:id",getVideo)
+
+router.get("/user/find/:id",getVideos)
+
+router.put("/view/:id",addView)
+
+router.get("/tags",getByTags)
+
+router.get("/select",getBySelect)
+
+router.get("/search",search)
+
+router.get("/random",random)
+
+router.get("/trend",trend)
+
+router.get("/sub",verifyToken,sub)
+
+
+export default router;
