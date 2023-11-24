1ddd2886c8e34da9a37d1ce15fe5346ab893fa2d
Vedant <vedanthelwatkar@gmail.com>
2023-11-25 00:01:14 +0530

    commit

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -92,19 +92,19 @@ export const Card = ({ type, video }) => {
     }
   }, [video.userId]);
   const { currentVideo } = useSelector((state) => state.video);
-  const incViews = async () => {
-    await axios.put(
-      `https://vtube-ycci.onrender.com/api/videos/view/${currentVideo._id}`,
-      {
-        headers: {
-          "Access-Control-Allow-Credentials": "true" ,
-          "Access-Control-Allow-Origin": "*" ,
-          "Access-Control-Allow-Methods":"GET,OPTIONS,PATCH,DELETE,POST,PUT",
-          "Access-Control-Allow-Headers":"X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        }
-    );
-  }
+  // const incViews = async () => {
+  //   await axios.put(
+  //     `https://vtube-ycci.onrender.com/api/videos/view/${currentVideo._id}`,
+  //     {
+  //       headers: {
+  //         "Access-Control-Allow-Credentials": "true" ,
+  //         "Access-Control-Allow-Origin": "*" ,
+  //         "Access-Control-Allow-Methods":"GET,OPTIONS,PATCH,DELETE,POST,PUT",
+  //         "Access-Control-Allow-Headers":"X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+  //         },
+  //       }
+  //   );
+  // }
 
 
   return (
