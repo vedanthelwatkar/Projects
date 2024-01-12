335aec90bc93fdc8063d1446e4be61ef7cbbf8bb
Vedant <vedanthelwatkar@gmail.com>
2024-01-12 16:09:31 +0530

    commit

--- sculpt-frontend/src/components/Register.js
+++ sculpt-frontend/src/components/Register.js
@@ -19,7 +19,7 @@ function Register(props) {
     const handleSubmit = async (e) => {
         e.preventDefault();
         const { firstName, lastName, number, email, password } = credentials; // Destructuring
-        const response = await fetch(`http://localhost:5000/api/auth/createuser`, {
+        const response = await fetch(`https://sculpt.onrender.com/api/auth/createuser`, {
             method: 'POST', // *GET, POST, PUT, DELETE, etc.
             headers: {
                 'Content-Type': 'application/json',
